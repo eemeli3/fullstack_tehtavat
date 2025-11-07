87a609b11d3b8ed86003c40f8556fc74e5daa478
Eemeli <sampo.koivu@gmail.com>
2025-11-07 16:04:21 +0200

    uibuild

--- osa3/puhelinluetteloBackend/index.js
+++ osa3/puhelinluetteloBackend/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
-app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (request, response) => {
     const body = request.body
