b0b257d9ef2bc2c27d7a8ccf0358796579b8ab07
Eemeli <sampo.koivu@gmail.com>
2025-11-12 15:42:50 +0200

    commit message

---
+++ osa3/puhelinluetteloBackend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.nca5f8k.mongodb.net/puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    console.log("phonebook:")
+
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+    //displayPersons()
+} else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log('person saved')
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Wrong number of arguments. Use "node mongo.js <password>" to display persons in phonebook or "node mongo.js <password> <name> <number>" to add person to phonebook.')
+    process.exit(1)
+}
+
