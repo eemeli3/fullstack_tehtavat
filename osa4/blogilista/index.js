db6808ce4b7d113c672aee27a055f2e58685bfe1
Eemeli <sampo.koivu@gmail.com>
2025-11-19 13:09:54 +0200

    app.js added

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,36 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
 const { info, error } = require('./utils/logger')
 const { mongoUrl, PORT } = require('./utils/config')
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 app.listen(PORT, () => {
   info(`Server running on port ${PORT}`)
