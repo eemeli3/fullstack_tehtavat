a1c5de367dcc08b03f3eeda205e0c47bc8fa94cf
Eemeli <sampo.koivu@gmail.com>
2025-11-19 13:29:44 +0200

    blogs controller and blog model added

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,34 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const { info, error } = require('./utils/logger')
 const { mongoUrl, PORT } = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
